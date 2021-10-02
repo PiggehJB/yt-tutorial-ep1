d0035ba796959d8980696dd5ff949e2ebc009c51
PiggehJB <90732485+PiggehJB@users.noreply.github.com>
2021-10-02 17:21:52 -0500

    Create ep1.py

---
+++ ep1.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+token = "ODkzOTY4NzE4MDYzODc4MjM1.YVjLPw.gC-rb7kmjb0ZT4uOkcWbyoemcCQ"
+prefix = "-"
+
+bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+    print("The bot is online!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+bot.run(token)
+© 2021 GitHub, Inc.
+Terms
