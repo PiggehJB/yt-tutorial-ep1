307c53dcc5d539f7e19f1a858dab3ad61a04f0d4
PiggehJB <90732485+PiggehJB@users.noreply.github.com>
2021-10-15 23:08:28 -0500

    Update ep7.py

--- ep7.py
+++ ep7.py
@@ -1,1 +1,21 @@
-Soon
+import discord
+from discord.ext import commands
+# Your token
+token = "Token here"
+prefix = "-"
+
+bot = commands.Bot(command_prefix=prefix)
+
+intents = discord.Intents.all()
+@bot.event
+async def on_ready():
+    print(f"{Fore.GREEN}The bot is online!")
+    
+@bot.event
+async def on_member_join(member):
+  role_name = "Member" # Your role's name, in my case it's 'Member'
+  role = discord.utils.get(member.guild.roles, name=role_name)
+  await member.add_roles(role)
+  print(f"Role added to {member}")
+  
+ bot.run(token)
